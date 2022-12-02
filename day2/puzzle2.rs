92be9979f197ffefa56b5d5f459858ff598be3e0
Thijs Oostdam <thijs.oostdam@nn.nl>
2022-12-02 09:30:49 +0100

    learning

--- day2/puzzle2.rs
+++ day2/puzzle2.rs
@@ -11,33 +11,33 @@ fn main() {
     let s = 3;
     // read file into type
     let score = read_lines("./input.txt")
-        // unwrap the result to the Ok value.  
+        // unwrap the result to the Ok value. (which is type String)
         .map(|result| result.unwrap())
         // splitting the line and taking ownership of the value.
-        // we need ownership as the line goes out of scope and we want to keep using it.
-        //TODO: is this true? to_owned probably allocates new heap space, which is not required as we only read.
+        // we need ownership as the line goes out of scope and we want to keep using it. This will allocate space on the heap.
         // we need to specify the type to collect it in,
         // but for the the type of the collection we do not need to specify the type
         // we could type Vec<String>
         .map(|line| line.split(" ").map(str::to_owned).collect::<Vec<_>>())
-        // map to string references
-        .map(|hands| )
         // map the text input resembling the hands that need to be played to a score
         .map(|hands| {
-            // map into a tuple containing references, as we can compare these to literals.
-            let tup = (hands[0].as_str(), hands[1].as_str());
-            // match expression returns a u32
-            match tup {
-                ("A", "X") => loss + s,
-                ("A", "Y") => draw + r,
-                ("A", "Z") => win + p,
-                ("B", "X") => loss + r,
-                ("B", "Y") => draw + p,
-                ("B", "Z") => win + s,
-                ("C", "X") => loss + p,
-                ("C", "Y") => draw + s,
-                ("C", "Z") => win + r,
-                _ => panic!("unknown combo {:?}", tup),
+            // https://stackoverflow.com/questions/41179659/convert-vecstring-into-a-slice-of-str-in-rust
+            // this stackoverflow nice explains the options we have to go from a Vec<String> to a Vec<&str>
+            let refs = hands.iter().map(String::as_str).collect::<Vec<_>>();
+            // &refs[..] turns the Vec into a slice
+            // so does refs.as_slice()
+            // match expression returns a u32 (the default)
+            match refs.as_slice() {
+                ["A", "X"] => loss + s,
+                ["A", "Y"] => draw + r,
+                ["A", "Z"] => win + p,
+                ["B", "X"] => loss + r,
+                ["B", "Y"] => draw + p,
+                ["B", "Z"] => win + s,
+                ["C", "X"] => loss + p,
+                ["C", "Y"] => draw + s,
+                ["C", "Z"] => win + r,
+                _ => panic!("unknown combo {:?}", refs),
             }
         })
         .sum::<u32>();
@@ -62,6 +62,6 @@ where
     }
 }
 
-// fn type_of<T>(_: T) -> &'static str {
-//     std::any::type_name::<T>()
-// }
+fn type_of<T>(_: T) -> &'static str {
+    std::any::type_name::<T>()
+}
